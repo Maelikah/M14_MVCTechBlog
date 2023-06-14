5e7c565ec3a9f7d4a8b6861b838b57eba32a86c9
Maelikah <eva.longoria@gmail.com>
2023-06-14 12:56:55 -0600

    Created logic and files for dashboard route

---
+++ controllers/dashboardRoutes.js
@@ -0,0 +1,25 @@
+const router = require('express').Router();
+const { Blog, User, Comment } = require('../models');
+const withAuth = require('../utils/auth');
+
+//Get blog posts that need to be edited or deleted by id
+router.get('/edit/:id', async (req, res) => {
+    try {
+        const blogUpdate = await Blog.findByPk(req.params.id);
+
+        if(blogUpdate) {
+            
+            const blog = blogUpdate.get({plain: true});
+            res.render('edit', {
+                ...blog,
+                logged_in: true
+            });
+        }
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
+
+module.exports = router;
